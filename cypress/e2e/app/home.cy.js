4df8ff74e823b2e6a38e3a200a08d52358d8d4b5
Jon Colque <jon.colque@fierastudio.com>
2022-12-01 17:59:23 -0300

    Add about me and footer description

--- cypress/e2e/app/home.cy.js
+++ cypress/e2e/app/home.cy.js
@@ -13,15 +13,13 @@
 
 describe('home', () => {
   beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
     cy.visit('localhost:3000')
   })
 
-  it('display name', () => {
+  it('display name, profession, aboutme', () => {
     cy.get('[data-test=title-name]').should('contain.text', 'Jon Wilson Colque Limachi')
+    cy.get('[data-test=subtitle-profession]').should('contain.text', 'Software Engineer')
+    cy.get('[data-test=p-aboutme]').should('contain.text', 'I am passionate about the transmission of human knowledge, especially when it is technology. I am methodical, I focus on delivering value, I like teamwork. Systems Engineer.')
   })
 
 })
