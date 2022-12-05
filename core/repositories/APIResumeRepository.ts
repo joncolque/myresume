1325da9d37dca0396913f91e8fe0fa7f81fe5d28
Jon Colque <jon.colque@fierastudio.com>
2022-12-05 15:32:19 -0300

    Fix job description and add name header of tab page

--- core/repositories/APIResumeRepository.ts
+++ core/repositories/APIResumeRepository.ts
@@ -28,12 +28,21 @@ const resume: Resume = {
         {
           name: 'Mobile Developer',
           years: '2020',
-          desc: 'Responsible for all stages of the apps. Code reviewer. Use clean architecture. Develop core integrations. Technical interviews.',
+          desc: 'Responsibilities: Development of the new Mobile department. Train new collaborators. Documentation. Technologies & Skills: React Native, Firebase services, environment settings, deployments PlayStore & AppleStore. Given workshops: React & React Native. Secondary skills: Kotlin for Android, Swift configurations for iOS, Go and NodeJS for microservices and proofs of concept for mobile features on backend side.',
         },
         {
           name: 'Fullstack developer',
           years: '2017 - 2020',
-          desc: 'React Native & React. Nextjs. CI. Typescript. Clean Architecture',
+          desc: `Development of apps for internal use by the company and for clients. Research of new technologies and their potential for the company. Exhibition of capitalized technologies to the development areas and technical support.
+          Technologies & frameworks: Angular, React, Node, Express, Golang, Firebase, MongoDB, Docker, Electron.
+          Agile methodologies: Scrum, TDD.
+          Given workshops: Angular, Go.
+          Projects:
+          - Microservices to connect devices regardless of protocol (IOT)
+          - Microservice to follow trucks with GPS
+          - CLI Framework javascript fullstack to generate applications end to end.
+          - Desktop application for public tenders of "Compr.ar" systems from National Goverment.
+          - Wiki repository to document the technological solutions for the company.`,
         }
       ]
     },
