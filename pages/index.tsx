b9dad132c187f2155e8a60e72b0a94a76fbebc63
Jon Colque <jon.colque@fierastudio.com>
2022-12-07 18:21:20 -0300

    Add social media and some icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,11 +10,11 @@ export default function Home() {
       <Head>
         <title>Jon Wilson Colque Limachi</title>
         <meta name="description" content="Software Engineer" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/j.png" />
       </Head>
 
       {resume && <main className={styles.main}>
-        <img className={styles.img_profile} src="https://media-exp1.licdn.com/dms/image/C4E03AQE1aCQkKfWCHQ/profile-displayphoto-shrink_800_800/0/1579644737769?e=1675296000&v=beta&t=98Dy2W8yZQI8eczWMFxObGYMvAW1ujbiQYBTMT0A644" alt="Girl in a jacket" width="500" height="600" />
+        <img className={styles.img_profile} src="https://media-exp1.licdn.com/dms/image/C4E03AQE1aCQkKfWCHQ/profile-displayphoto-shrink_800_800/0/1579644737769?e=1675296000&v=beta&t=98Dy2W8yZQI8eczWMFxObGYMvAW1ujbiQYBTMT0A644" alt="software engineer" width="500" height="600" />
         <div className={styles.info_profile}>
           <h1 className={styles.title} data-test={'title-name'}>
             {resume.name}
@@ -36,8 +36,18 @@ export default function Home() {
           </div>)}
         </p>)}
       </section>}
+      <section style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center', marginTop: '100px', marginBottom:'20px'}}>
+        <div style={{ justifyContent: 'center' }}>
+          <img src="gmail.svg" width={'20px'} style={{ marginRight: '4px' }} />
+          <text>joncolque@gmail.com</text>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <img src="linkedin.svg" width={'20px'} style={{ marginRight: '4px' }} />
+          <a target={'_blank'} href='https://www.linkedin.com/in/jon-wilson-colque-limachi-0439abb2' rel="noreferrer">joncolque</a>
+        </div>
+      </section>
       <footer className={styles.footer}>
-        <p>This page is a proof of concept builded with React on Nextjs to use: Clean architecture. Semantic html and css. Custom domain. AWS Amplify. AWS Route 53. And will be modified to test several concepts. Last updated: 5 DEC 2022. Cooming soon: Social media links, headless cypress, among others.</p>
+        <p>This page is a proof of concept builded with React on Nextjs to use: Clean architecture. Semantic html and css. Custom domain. AWS Amplify. AWS Route 53. And will be modified to test several concepts. Last updated: 7 DEC 2022. Cooming soon: headless cypress,  git repository, among others.</p>
       </footer>
     </div>
   )
