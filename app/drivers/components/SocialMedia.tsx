2fb7d2f83269766ebe8e4c1ef3527525265f602b
Jon Colque <jon.colque@fierastudio.com>
2022-12-27 12:35:26 -0300

    Fix css on social media component

--- app/drivers/components/SocialMedia.tsx
+++ app/drivers/components/SocialMedia.tsx
@@ -5,15 +5,15 @@ const linkedin = 'https://www.linkedin.com/in/jon-wilson-colque-limachi-0439abb2
 const github = 'https://github.com/joncolque/myresume'
 
 export const SocialMedia = () => <section className={styles.container}>
-    <div style={{ justifyContent: 'center' }}>
+    <div >
         <img src="gmail.svg" width={'20px'} style={{ marginRight: '4px' }} />
         <text>{email}</text>
     </div>
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <div >
         <img src="linkedin.svg" width={'20px'} style={{ marginRight: '4px' }} />
         <a target={'_blank'} href={linkedin} rel="noreferrer">joncolque</a>
     </div>
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <div >
         <img src="github.svg" width={'20px'} style={{ marginRight: '4px' }} />
         <a target={'_blank'} href={github} rel="noreferrer">joncolque</a>
     </div>
