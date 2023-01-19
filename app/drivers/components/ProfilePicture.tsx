50c5113267411c89194587fef731cb38c26f34b5
Jon Colque <jon.colque@fierastudio.com>
2023-01-19 00:38:23 -0300

    Fix new photo and change backcground from white to whitesmoke

--- app/drivers/components/ProfilePicture.tsx
+++ app/drivers/components/ProfilePicture.tsx
@@ -2,7 +2,7 @@ import styles from './profilePicture.module.css'
 
 export const ProfilePicture = ()=><img
     className={styles.img_profile}
-    src="https://media-exp1.licdn.com/dms/image/C4E03AQE1aCQkKfWCHQ/profile-displayphoto-shrink_800_800/0/1579644737769?e=1675296000&v=beta&t=98Dy2W8yZQI8eczWMFxObGYMvAW1ujbiQYBTMT0A644"
+    src="https://media.licdn.com/dms/image/D4D03AQGJws2b3yWdDw/profile-displayphoto-shrink_800_800/0/1673998885539?e=1679529600&v=beta&t=Zj3kYq4oEZrXjep7kc-GzhHP2Y-1IPlTiJX3wM6qFjo"
     alt="software engineer"
     width="500"
     height="600"
