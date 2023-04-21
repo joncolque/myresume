b023d8a3ac0888dc8f9206ce2b842ea2c3eb0903
joncolque <joncolque@gmail.com>
2023-04-21 01:26:54 -0300

    Add pdf downloader

--- app/drivers/components/SocialMedia.tsx
+++ app/drivers/components/SocialMedia.tsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import { Tooltip } from '../ui-elements/Tooltip'
 import styles from './socialmedia.module.css'
-import {socialMedia, email} from '../../../src-data/socialMedia'
+import { socialMedia, email } from '../../../src-data/socialMedia'
+import { Resume } from '../../../core/entities/Resume'
+import DownloadResumePDF from './PDF/DownloadResumePDF'
+import { MyResumePDF } from './PDF/MyResumePDF'
 
 const defaultTooltipText = 'Click to copy: ' + email
 const copiedTooltipText = 'Copied!'
 const iconSize = '40px'
 const iconGmailSrc = 'gmail.svg'
 
-export const SocialMedia = () => {
+interface Props {
+    resume: Resume
+}
+
+export const SocialMedia = ({ resume }: Props) => {
     const [tooltipText, setTooltipText] = useState<string>(defaultTooltipText)
 
     const onClickEmailIcon = () => {
@@ -20,8 +27,8 @@ export const SocialMedia = () => {
     }
 
     return <section className={styles.container}>
-        {socialMedia.map(sm=><a key={sm.name} target={'_blank'} href={sm.url} rel='noreferrer'>
-            <img src={sm.icon} width={iconSize}  data-test={sm.name}/>
+        {socialMedia.map(sm => <a key={sm.name} target={'_blank'} href={sm.url} rel='noreferrer'>
+            <img src={sm.icon} width={iconSize} data-test={sm.name} />
         </a>)}
         <Tooltip text={tooltipText}>
             <img
@@ -32,5 +39,6 @@ export const SocialMedia = () => {
                 data-test={'email'}
             />
         </Tooltip>
+        {resume && <DownloadResumePDF><MyResumePDF resume={resume} /></DownloadResumePDF>}
     </section>
 }
